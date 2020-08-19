0a69c2f0c4ab501c7d6f5b01bd4e3a482e49d611
nilesh <nileshbuchade13@gmail.com>
2020-08-19 05:40:56 +0530

    Updates

---
+++ precache-manifest.1e022a75ee526a3fa6a7b06de646f710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffad29661cbfc3c9e42813fdbcac487",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "aaa4fc4fd14ad73e8ac0",
+    "url": "/netflix-clone/static/css/main.f3a6cf51.chunk.css"
+  },
+  {
+    "revision": "099d0d20bdd08c1e3947",
+    "url": "/netflix-clone/static/js/2.f5bd01e2.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.f5bd01e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa4fc4fd14ad73e8ac0",
+    "url": "/netflix-clone/static/js/main.f3476243.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
